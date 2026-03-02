439b72a8fafa13f9f920963008b7a81c30cc657d
Enzo-Silveira <enzoleranhe@gmail.com>
2026-03-02 09:25:20 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Banner from './componentes/banner/banner';
+import Formulario from './componentes/Formulario/Formulario';
+
+function App() {
+  
+
+  return (
+    <div>
+      <Banner />
+      <Formulario/>
+    </div>
+  )
+}
+
+export default App
